6547c48213063e8c78151947a3bee85f36062daf
qoxicization <167058955+qoxicization@users.noreply.github.com>
2024-04-15 12:54:43 +0300

    Add files via upload

---
+++ PM01_020_Ermokhin111/PM01_020_Ermokhin/MenuOrganizatorov.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PM01_020_Ermokhin
+{
+    /// <summary>
+    /// Логика взаимодействия для MenuOrganizatorov.xaml
+    /// </summary>
+    public partial class MenuOrganizatorov : Window
+    {
+        public MenuOrganizatorov()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
